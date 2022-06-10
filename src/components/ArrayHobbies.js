2e68d9eadd1650a7bc854f6f7d5d13373f7f41e6
Chaplin16 <chaplin16@orange.fr>
2022-06-10 11:05:50 +0200

    titre sur cards hobbies

--- src/components/ArrayHobbies.js
+++ src/components/ArrayHobbies.js
@@ -1,82 +1,92 @@
 const hobbies = {
-  handiwork: [
-    {
-      src: "./assets/hobbies/disqueuse-montant.png",
-      alt: "",
-    },
-    {
-      src: "./assets/hobbies/ouverture-mur-porteur.png",
-      alt: "",
-    },
-    {
-      src: "./assets/hobbies/planche-coffrage-scie-sabre.png",
-      alt: "",
-    },
-    {
-      src: "./assets/hobbies/ponceuse.jpg",
-      alt: "",
-    },
-    {
-      src: "./assets/hobbies/decoupe-isolant.png",
-      alt: "",
-    },
-    {
-      src: "./assets/hobbies/isolation-laine-de-bois.jpg",
-      alt: "",
-    },
-  ],
-  sport: [
-    {
-      src: "./assets/hobbies/direct-sac-de-frappe.jpg",
-      alt: "",
-    },
-    {
-      src: "./assets/hobbies/developpe-couché.jpg",
-      alt: "",
-    },
-    {
-      src: "./assets/hobbies/traction-avec-elastique.png",
-      alt: "",
-    },
-    {
-      src: "./assets/hobbies/peck-deck.png",
-      alt: "",
-    },
-    {
-      src: "./assets/hobbies/monter-de-box.jpg",
-      alt: "",
-    },
-    {
-      src: "./assets/hobbies/developpe-couché.jpg",
-      alt: "",
-    }
-  ],
-  caps : [
-    {
-      src:"./assets/hobbies/caps-pinUp.png",
-      alt:""
-    },
-    {
-      src:"./assets/hobbies/caps-parisenne.png",
-      alt:""
-    },
-    {
-      src:"./assets/hobbies/caps-LALLEMENT.png",
-      alt:""
-    },
-    {
-      src:"./assets/hobbies/caps-de-gaulle.png",
-      alt:""
-    },
-    {
-      src:"./assets/hobbies/caps-sonnet-gillot.png",
-      alt:""
-    },
-    {
-      src:"./assets/hobbies/caps-strauss-gorgeton.png",
-      alt:""
-    },
-  ]
-};
+  handiwork: {
+    title: 'Bricolage',
+    image : [
+      {
+        src: "./assets/hobbies/disqueuse-montant.png",
+        alt: "",
+      },
+      {
+        src: "./assets/hobbies/ouverture-mur-porteur.png",
+        alt: "",
+      },
+      {
+        src: "./assets/hobbies/planche-coffrage-scie-sabre.png",
+        alt: "",
+      },
+      {
+        src: "./assets/hobbies/ponceuse.jpg",
+        alt: "",
+      },
+      {
+        src: "./assets/hobbies/decoupe-isolant.png",
+        alt: "",
+      },
+      {
+        src: "./assets/hobbies/isolation-laine-de-bois.jpg",
+        alt: "",
+      },
+    ]
+  },
+  sport: {
+    title: 'Sport',
+    image:  [
+      {
+        src: "./assets/hobbies/direct-sac-de-frappe.jpg",
+        alt: "",
+      },
+      {
+        src: "./assets/hobbies/developpe-couché.jpg",
+        alt: "",
+      },
+      {
+        src: "./assets/hobbies/traction-avec-elastique.png",
+        alt: "",
+      },
+      {
+        src: "./assets/hobbies/peck-deck.png",
+        alt: "",
+      },
+      {
+        src: "./assets/hobbies/monter-de-box.jpg",
+        alt: "",
+      },
+      {
+        src: "./assets/hobbies/developpe-couché.jpg",
+        alt: "",
+      }
+    ]
+  },   
+  caps : {
+    title: 'Placomusophile',
+    image: [
+      {
+        src:"./assets/hobbies/caps-pinUp.png",
+        alt:""
+      },
+      {
+        src:"./assets/hobbies/caps-parisenne.png",
+        alt:""
+      },
+      {
+        src:"./assets/hobbies/caps-LALLEMENT.png",
+        alt:""
+      },
+      {
+        src:"./assets/hobbies/caps-de-gaulle.png",
+        alt:""
+      },
+      {
+        src:"./assets/hobbies/caps-sonnet-gillot.png",
+        alt:""
+      },
+      {
+        src:"./assets/hobbies/caps-strauss-gorgeton.png",
+        alt:""
+      },
+    ]
+  }
+}
+
 
 export default hobbies;
--- src/components/CardHobbies.js
+++ src/components/CardHobbies.js
@@ -3,9 +3,9 @@ export default function CardHobbies(props) {
     <main>        
             <div className="card m-2 mb-5">
             
-              <h3 className="card-title text-center pt-3">Bricolage</h3>
+              <h3 className="card-title text-center pt-3">{props.hobbies.title}</h3>
               <div className="card-body">
-                  {props.hobbies.map((photo,index) => [
+                  {props.hobbies.image.map((photo,index) => [
                       <img key= {index} src={photo.src} alt={photo.alt} />
                   ])}
                     
