361008467dd84c7d25e079fe5c0464041bf9e3bf
Chaplin16 <chaplin16@orange.fr>
2022-06-15 18:18:17 +0200

    add text

--- src/components/Text.js
+++ src/components/Text.js
@@ -2,8 +2,8 @@ export default function Text() {
   return (
     <div className='text text-align-left pt-3 pe-4 mb-5 w-100 content'>
         <p>Passionée par le developpement Web, ce que je préfère c'est comprendre comment cela fontionne. Découvrir, apprendre et créer, je viens de résumer ma vie 😃. Front-end, Back-end, design d'interface, nouvelles fonctionnalités, SEO, étant freelance, toutes les étapes doivent être maîtrisées et améliorées sans cesse pour satisfaire pleinement les demandes des clients.</p>
-        <p>Développeur junior depuis un an, une trentaine de sites vitrines réalisés, je sais que seuls l'expérience et la pratique sur différents projets de plus en plus complexes m'amèneront au <b>Graal</b>.</p>
-        <p>Intégrer une entreprise en stage plusieurs mois pour travailler sur des projets, créer, tester, découvrir, faire et refaire jusqu'à résoudre les bugs, en back, front ou en full-stack, voilà mon ambition pour septembre 2022.</p>
+        <p>Développeur junior depuis un an, une trentaine de sites vitrines réalisés, des maquettes figma codées sans ou avec framework, je sais que seuls l'expérience et la pratique sur différents projets de plus en plus complexes m'amèneront au <b>Graal</b>.</p>
+        <p><strong>Intégrer une entreprise en stage plusieurs mois</strong> pour travailler sur des projets, créer, tester, découvrir, faire et refaire jusqu'à résoudre les bugs, en back, front ou en full-stack, voilà mon ambition pour septembre 2022.</p>
         <p>Freelance, je facture au tarif stagiaire à l'entreprise qui me fera confiance. Je travaille en télétravail excepté le mercredi matin.</p>
     </div>
   )
