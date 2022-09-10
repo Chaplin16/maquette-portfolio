07fc1592999ab2912b27c70d1fd9a8db6543950f
Chaplin16 <chaplin16@orange.fr>
2022-09-11 00:36:04 +0200

    add text in text.js

--- src/components/Text.js
+++ src/components/Text.js
@@ -5,22 +5,18 @@ export default function Text() {
   return (
     <div className="text">
         <h2 className='text-center mb-5'>
-            Aurore <br/>developpeuse web junior ⭐<br/>télétravail  - rémunération au smic <br/> 
-            <span className='fw-bold'>pour accroître mes compétences</span>
+            Aurore <br/>⭐ intégrateur web ⭐<br/>télétravail
+           
         </h2>
         <ul className='service'>
             <li className='d-flex align-items-center my-2'>
                 <img src="./assets/icon/aurore-check.png" alt="" />
-                <p className='m-0 ps-3 lead'> soit en Front-end HTML CSS JS BOOTSTRAP SASS REACT </p>
+                <p className='m-0 ps-3 lead'> Intégration de maquettes en HTML CSS JS BOOTSTRAP SASS REACT </p>
             </li>
             <li className='d-flex align-items-center my-2'>
                 <img src="./assets/icon/aurore-check.png" alt="" />
-                <p className='m-0 ps-3 lead'>soit en Back-end avec NODE.JS SEQUELIZE EXPRESS</p>
-            </li>
-            <li className='d-flex align-items-center my-2 '>
-                <img src="./assets/icon/aurore-check.png" alt="" />
-                <p className='m-0 ps-3 lead'>avec API et base de données SQL et NoSQL</p>
-            </li>
+                <p className='m-0 ps-3 lead'>Notions en Back-end avec NODE.JS   SEQUELIZE  EXPRESS <br/>avec API et base de données SQL et NoSQL</p>
+            </li>   
         </ul>
     </div>
   )
