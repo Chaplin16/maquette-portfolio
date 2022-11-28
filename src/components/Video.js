bf2a8848535fdfa53cb58f78e5374e936be48eb8
Chaplin16 <chaplin16@orange.fr>
2022-11-28 22:44:08 +0100

    change video header

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,22 +2,23 @@ import { useEffect,useState } from 'react';
 
 //function pour faire apparaitre a interval de 7s la deuxieme video puis inversement 7s plus tard
 export default function Video() {
-    const [source, setSource] = useState('video-aurore-1.mp4');
+    const [source, setSource] = useState('video-pour-sous-traitance-dev.mp4');
 
-    useEffect(() => {
-        const video = document.getElementById('video');
-        setInterval(() => {
-            if (source === 'video-aurore-1.mp4')
-                setSource('video-aurore-2.mp4');
-            else
-                setSource('video-aurore-1.mp4')
-            video.load();
-        }, 8200);
-    },[source]);
 
-    return (
-        <video autoPlay muted id="video">
-            <source src={`/video/${source}`} type="video/mp4"></source>
-        </video>
-    )
+useEffect(() => {
+    const video = document.getElementById('video');
+    setInterval(() => {
+        if (source === 'video-pour-sous-traitance-dev-2.mp4')
+            setSource('video-pour-sous-traitance-dev.mp4');
+        else
+            setSource('video-pour-sous-traitance-dev-2.mp4')
+        video.load();
+    }, 8200);
+},[source]);
+
+return (
+    <video autoPlay muted id="video">
+        <source src={`/video/${source}`} type="video/mp4"></source>
+    </video>
+)
 }
